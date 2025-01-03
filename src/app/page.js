5bc55bf86da161be57a953a3dbc364b67ab57708
Paulo Gabriel <braido350@gmail.com>
2025-01-03 15:48:38 -0400

    ajustes na pagina principal

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,12 @@
 import React from 'react';
-import Header from '@/components/header';
-import MainHome from '@/components/mainHome';
-import Testes from '@/components/testes';
+import MainHome from '@/components/vendas';
 
 export default function Home() {
   return (
-    <div className='w-screen min-h-screen flex flex-col justify-between items-center'>
-      <div className='container flex justify-center items-center w-full h-full'>
+    <div className='w-full h-full'>
+      <div className='flex justify-center itens-center'>
         <MainHome/>
       </div>
-      <Testes/>
     </div>
   );
 }
