700ba45ec2a575b2aa2857b1d0b058bd7a965491
Paulo Gabriel <braido350@gmail.com>
2025-02-25 14:35:56 -0400

    refatora importações de componentes CadastroClientes e CadastroProdutos para caminhos relativos

--- src/app/cadastro/clientes/page.js
+++ src/app/cadastro/clientes/page.js
@@ -1,10 +1,6 @@
 "use client";
 import React from "react";
-<<<<<<< HEAD
-import CadastroClientes from "../../cadastro/clientes/Clientes";
-=======
 import CadastroClientes from "./Clientes";
->>>>>>> b1ee5f7 (refatora autenticação para usar axios, melhora tratamento de erros e ajusta estrutura de componentes)
 
 export default function cadastro() {
   return (
--- src/app/cadastro/produtos/page.js
+++ src/app/cadastro/produtos/page.js
@@ -1,10 +1,6 @@
 "use client";
 import React from "react";
-<<<<<<< HEAD
-import CadastroProdutos from "../../cadastro/produtos/Produtos";
-=======
 import CadastroProdutos from "./Produtos";
->>>>>>> b1ee5f7 (refatora autenticação para usar axios, melhora tratamento de erros e ajusta estrutura de componentes)
 
 export default function cadastro() {
   return (
